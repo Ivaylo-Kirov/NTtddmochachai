a656d0a327ef6bf75a36d20b4b8dffc57c2409bc
Ivaylo-Kirov <ivo.kirov@gmail.com>
2019-10-03 17:24:49 -0400

    Added subtract

--- test/testfunctions.js
+++ test/testfunctions.js
@@ -1,8 +1,14 @@
-const sum = require('../functions');
+const funcs = require('../functions');
 const expect = require('chai').expect;
 
 describe('sum function', () => {
     it('should correctly sum 2 INTS', () => {
-        expect(sum(2, 3)).to.equal(5);
+        expect(funcs.sum(2, 3)).to.equal(5);
+    })
+});
+
+describe('subtract function', () => {
+    it('should correctly subtract between 2 INTs', () => {
+        expect(funcs.subtract(20, 3)).to.equal(17);
     })
 });
